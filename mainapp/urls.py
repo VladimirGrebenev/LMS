f64dd34817a0d0bfdc9de249469e9646a1b44ed6
VladimirGrebenev <grebenev-81@mail.ru>
2022-11-30 15:29:16 +0300

    lesson_5 init

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,24 +8,6 @@ app_name = MainappConfig.name
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
     path("news/", views.NewsPageView.as_view(), name="news"),
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
-=======
->>>>>>> cb71671 (lesson_3 Urls in templates)
-=======
-    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
->>>>>>> b00f658 (lesson_3 Args in url)
-    path("courses/", views.CoursesPageView.as_view(), name="courses"),
-=======
-=======
->>>>>>> 949da51 (Lesson 3 (#2))
-=======
->>>>>>> c323222 (lesson_4 Display model's data dotenv secretkey)
     path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
     path("courses/", views.CoursesListView.as_view(), name="courses"),
     path(
@@ -33,21 +15,6 @@ urlpatterns = [
         views.CoursesDetailView.as_view(),
         name="courses_detail",
     ),
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 202c2e9 (lesson_4 Display model's data)
-=======
-=======
-    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
-    path("courses/", views.CoursesPageView.as_view(), name="courses"),
->>>>>>> 9044c7d (Lesson 3 (#2))
->>>>>>> 949da51 (Lesson 3 (#2))
-=======
-    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
-    path("courses/", views.CoursesPageView.as_view(), name="courses"),
->>>>>>> e422349 (Lesson 3 (#2))
-=======
->>>>>>> c323222 (lesson_4 Display model's data dotenv secretkey)
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
     path("login/", views.LoginPageView.as_view(), name="login"),
--- mainapp/views.py
+++ mainapp/views.py
@@ -11,55 +11,11 @@ class MainPageView(TemplateView):
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 31f4ee1 (Lesson 3 (#2))
     def get_context_data(self, **kwargs):
         # Get all previous data
         context = super().get_context_data(**kwargs)
         # Create your own data
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        context["news_title"] = "Громкий новостной заголовок"
-        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
-<<<<<<< HEAD
-<<<<<<< HEAD
-        context["range"] = range(5)
-        context["datetime_obj"] = datetime.now()
-<<<<<<< HEAD
-        return context
-
-
-class NewsWithPaginatorView(NewsPageView):
-    def get_context_data(self, page, **kwargs):
-        context = super().get_context_data(page=page, **kwargs)
-        context["page_num"] = page
-=======
->>>>>>> de2bc33 (lesson_3 Template inhereting)
-=======
-        context["range"] = range(5)
->>>>>>> 8b40449 (lesson_3 Loops in templates)
-=======
->>>>>>> 50e23b6 (lesson_3 Template's filters)
-=======
-=======
->>>>>>> 949da51 (Lesson 3 (#2))
         context["news_qs"] = mainapp_models.News.objects.all()[:5]
->>>>>>> 202c2e9 (lesson_4 Display model's data)
-        return context
-=======
-=======
->>>>>>> 31f4ee1 (Lesson 3 (#2))
-        context["news_title"] = "Громкий новостной заголовок"
-        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
-        context["range"] = range(5)
-        context["datetime_obj"] = datetime.now()
-=======
-        context["news_qs"] = mainapp_models.News.objects.all()[:5]
->>>>>>> c323222 (lesson_4 Display model's data dotenv secretkey)
         return context
 
 
@@ -71,22 +27,6 @@ class NewsPageDetailView(TemplateView):
         context["news_object"] = get_object_or_404(mainapp_models.News, pk=pk)
         return context
 
-<<<<<<< HEAD
->>>>>>> 9044c7d (Lesson 3 (#2))
-
-
-class NewsPageDetailView(TemplateView):
-    template_name = "mainapp/news_detail.html"
-
-    def get_context_data(self, pk=None, **kwargs):
-        context = super().get_context_data(pk=pk, **kwargs)
-        context["news_object"] = get_object_or_404(mainapp_models.News, pk=pk)
-        return context
-
-=======
->>>>>>> e422349 (Lesson 3 (#2))
-=======
->>>>>>> 31f4ee1 (Lesson 3 (#2))
 
 class CoursesListView(TemplateView):
     template_name = "mainapp/courses_list.html"
