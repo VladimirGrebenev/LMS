792388438773fa4922e02a5efc62ea368426d950
Vladimir Grebenev <57130730+VladimirGrebenev@users.noreply.github.com>
2022-12-14 16:49:36 +0300

    Lesson 9 (#11)
    * lesson_9 Preporations for deployment
    
    * lesson_9 Preporations for deployment update
    
    * lesson_9 Preporations for deployment setting

--- config/settings.py
+++ config/settings.py
@@ -93,21 +93,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if os.getenv("ENV_TYPE") != "local":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "geekshop",
-            "USER": "postgres",
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 
 # Password validation
@@ -169,10 +160,9 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-if os.getenv("ENV_TYPE") != "local":
-    STATIC_ROOT = BASE_DIR / "static"
-else:
-    STATICFILES_DIRS = (BASE_DIR / "static/",)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Media files
 MEDIA_URL = "/media/"
