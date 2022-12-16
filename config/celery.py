5df40c411473d981b4b5d320e6cb7d788ccf856b
VladimirGrebenev <grebenev-81@mail.ru>
2022-12-16 11:08:48 +0300

    new secelery py

--- config/celery.py
+++ config/celery.py
@@ -1,11 +1,17 @@
 import os
 
 from celery import Celery
-from django.conf import settings
 
-if settings.DEBUG:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+# Путь к настройкам
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+# Название celery приложения
 celery_app = Celery("braniac")
+
+# Путь, откуда будет забираться конфигурация для celery приложения
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Автоподрузка задач
 celery_app.autodiscover_tasks()
+
+# celery -A Braniac worker -l info -- консольная команда для запуска celery, инфо- уровень вывода логов celery в консоль
